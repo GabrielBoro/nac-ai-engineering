085893e4a66015d2e6d7d5bfdb2f1266d5d07cca
GabrielBoro <61744698+GabrielBoro@users.noreply.github.com>
2022-03-14 20:44:03 -0300

    Atualização do código do terceiro exercício

--- nac_r3.py
+++ nac_r3.py
@@ -1,14 +1,18 @@
 # EXERCICIO 3
+
 # Faz o R2 e:
 
 # - Realiza o processamento com imagens da webcam (executa um programa .py) O resultado esperado é uma janela da OpenCV que exibe os contornos dos 2 círculos maiores, a reta entre centros dos círculos e o valor do ângulo em relação ao plano horizontal; (máx 2 pontos)
 
-# - Essa rubrica não pode ser feita no jupyter notebook ou google Colab. Deve ser um programa python .py. 
-
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
+import math
 
-def contornos(mask):
+
+def contornos(mask, frame):
+    gray = frame.copy()
+    gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
     contornos, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     centres = []
@@ -16,15 +20,14 @@ def contornos(mask):
         moments = cv.moments(contornos[i])
         centres.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
         
-    mask = cv.cvtColor(mask,cv.COLOR_GRAY2BGR)
 
-    img_rgb = cv.cvtColor(mask, cv.COLOR_BGR2RGB)
-    img_hsv = cv.cvtColor(mask, cv.COLOR_BGR2HSV)
+    img_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    img_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    contornos_img = mask.copy()
+    contornos_img = frame.copy()
 
-    image_lower_hsv = np.array([10, 10, 10])
-    image_upper_hsv = np.array([255, 255, 255])
+    image_lower_hsv = np.array([255, 0, 0])
+    image_upper_hsv = np.array([255, 0, 0])
 
     mask_hsv = cv.inRange(img_hsv, image_lower_hsv, image_upper_hsv)
 
@@ -35,48 +38,53 @@ def contornos(mask):
         cx2=centres[1][0]
         cy1=centres[0][1]
         cy2=centres[1][1]
-        cv.drawContours(contornos_img, contornos, -1, [255, 0, 0], 5)
-        cv.line(contornos_img,(cx1,cy1),(cx2, cy2),(255,0,0),5)
 
+        cv.drawContours(contornos_img, contornos, -1, [255, 255, 0], 5)
+        cv.line(contornos_img,(cx1,cy1),(cx2, cy2),(100,255,255),5)
+        m1 = (cy1 - cy2)/(cx1 - cx2)
+        m2 = (cy2 - cy2)/(cx1 - cx2)
+        angulo = math.atan((m2-m1)/(1-(m2*m1)))
+        angulo_graus = round(math.degrees(angulo))
+
+        font = cv.FONT_HERSHEY_SIMPLEX
+        text = f'Angulo da reta: {angulo_graus}'
+        textsize = cv.getTextSize(text, font, 1, 1)[0]
+
+        textX = int((contornos_img.shape[1] - textsize[0]) / 2)
+        textY = int((contornos_img.shape[0] + textsize[1]) / 2)
+        cv.putText(contornos_img, text, (textX, textY), font, 1, (100, 255, 255), 1)
         return contornos_img
     
-    return mask
+    return frame
 
 cv.namedWindow("preview")
 vc = cv.VideoCapture(0)
 
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():
     rval, frame = vc.read()
 else:
     rval = False
     
 while rval:
+    frame = cv.flip(frame, 1)
     cframe = frame.copy()
     cframe = cv.cvtColor(cframe,cv.COLOR_BGR2GRAY)
 
-    # cframe = cv.medianBlur(cframe,5)
-#     image: imagem de entrada na escala de ciza. 
-# -     method: Define o metódo de detecção de circulos.
-# -     dp: relação entre o tamanho da imagem e o tamanho do acumulador. Um dp grande "pega" bordas mais tênues.
-# - minDist: Distância minima entre centros (x,y) dos circulos detectados
-# - param1: Valor do gradiente usado para lidar com a detecção de bordas
-# - param2: Limiar do Acumulador usado pelo metódo. Se muito baixo, retorna mais circulos (incluindo círculos falsos). Se mais alto, mais círculos serão potencialmente retornados.
-# - minRadius: Raio minimo (em pixels).
-# - maxRadius: Raio máximo (em pixels).
-    circles = cv.HoughCircles(cframe,cv.HOUGH_GRADIENT,dp=5,minDist=200,param1=200,param2=200,minRadius=50,maxRadius=80)
+    circles = cv.HoughCircles(cframe,cv.HOUGH_GRADIENT,dp=5,minDist=200,param1=350,param2=250,minRadius=60,maxRadius=120)
 
     mask = cframe.copy()
+    mask = np.zeros(cframe.shape[:2],dtype="uint8")
 
     if circles is not None:
         raio = []
         circles = np.uint16(np.around(circles))
         for i in circles[0,:2]:
             cv.circle(mask,(i[0],i[1]),i[2],(255,255,255),-1)
-        mask = contornos(mask).copy()
+            cv.circle(frame,(i[0],i[1]),i[2],(255,255,255),5)
+        frame = contornos(mask, frame).copy()
 
-    mask = cv.flip(mask, 1)
-    cv.imshow("preview", mask)
+    cv.imshow("preview", frame)
     
     
     rval, frame = vc.read()
